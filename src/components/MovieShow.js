eba918d4dac654eece7d51ca657ce83265890e56
ferrisbueller66 <github email address>
2020-07-31 16:13:46 -0400

    Done.

---
+++ src/components/MovieShow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MovieShow = props => {
+    return (
+        <div>
+            <h3>Movies Show Component!</h3>
+        </div>
+    )
+}
+
+export default MovieShow
